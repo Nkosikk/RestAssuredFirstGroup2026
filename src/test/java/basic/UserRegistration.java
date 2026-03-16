5e96bf78c3fccad91441ef3ab766b4436769ad8e
Administrator <sibusiso.nkwanyana@simplify.africa>
2026-03-16 20:12:37 +0200

    init commit with added basic code

---
+++ src/test/java/basic/UserRegistration.java
@@ -0,0 +1,66 @@
+package basic;
+
+import com.github.javafaker.Faker;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UserRegistration {
+
+    static String authToken;
+    static String userId;
+    static String baseURL = "https://ndosiautomation.co.za";
+    @Test
+    public void adminLoginTest() {
+
+        String apiPath = "/APIDEV/login";
+        String payload = "{\n" +
+                "    \"email\": \"spare@admin.com\",\n" +
+                "    \"password\": \"@12345678\"\n" +
+                "}";
+
+        Response response = RestAssured.given()
+                .baseUri(baseURL)
+                .basePath(apiPath)
+                .header("Content-Type", "application/json")
+                .body(payload)
+                .log().all()
+                .post().prettyPeek();
+
+        int actualStatusCode = response.getStatusCode();
+        Assert.assertEquals(actualStatusCode, 200, "Status code should be 200");
+        authToken = response.jsonPath().getString("data.token");
+
+    }
+
+    @Test(priority = 2)
+    public void registerUser(){
+
+        String apiPath = "/APIDEV/register";
+        String registeredEmail = Faker.instance().internet().emailAddress();
+        String payload = "{\n" +
+                "    \"firstName\": \"dsfdsa\",\n" +
+                "    \"lastName\": \"sdfdsaf\",\n" +
+                "    \"email\": "+registeredEmail+",\n" +
+                "    \"password\": \"@a12345678\",\n" +
+                "    \"confirmPassword\": \"@a12345678\",\n" +
+                "    \"phone\": \"\",\n" +
+                "    \"groupId\": \"5328c91e-fc40-11f0-8e00-5000e6331276\"\n" +
+                "}";
+
+        Response response = RestAssured.given()
+                .baseUri(baseURL)
+                .basePath(apiPath)
+                .header("Content-Type", "application/json")
+                .header("Authorization", "Bearer " + authToken)
+                .body(payload)
+                .log().all()
+                .post().prettyPeek();
+
+        int actualStatusCode = response.getStatusCode();
+        Assert.assertEquals(actualStatusCode, 201, "Status code should be 201");
+        userId = response.jsonPath().getString("data.id");
+        System.out.println("Registered User ID: " + userId);
+    }
+}
