66effcdc77e62719ea6e89e786dfd757d3f6e2ad
refilwemakume <rchoche12@gmail.com>
2026-03-29 18:31:23 +0200

    Added Endpoints Class and ReadConfig Class to read from config property file

---
+++ src/test/java/constants/Endpoints.java
@@ -0,0 +1,16 @@
+package constants;
+
+import utils.ReadConfigPropertyFile;
+
+public class Endpoints {
+    // This class defines the API endpoints used in the tests.
+    // The endpoints are read from a configuration property file using the ReadConfigPropertyFile utility class.
+    public static String registerendpoint = ReadConfigPropertyFile.getProperty("api.registerendpoint");
+    public static String loginendpoint = ReadConfigPropertyFile.getProperty("api.loginendpoint");
+    public static String approveusersendpoint = ReadConfigPropertyFile.getProperty("api.approveusersendpoint");
+    public static String adminusersendpoint = ReadConfigPropertyFile.getProperty("api.adminusersendpoint");
+    public static String roleendpoint = ReadConfigPropertyFile.getProperty("api.roleendpoint");
+    public static String deleteuserendpoint = ReadConfigPropertyFile.getProperty("api.deleteuserendpoint");
+
+
+}
---
+++ src/test/java/utils/ReadConfigPropertyFile.java
@@ -0,0 +1,24 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadConfigPropertyFile {
+    private static Properties p;
+
+    static {
+        try {
+            FileInputStream fr = new FileInputStream(System.getProperty("user.dir") + "/src/test/resources/configfiles/config.properties");
+            p = new Properties();
+            p.load(fr);
+            fr.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String propertyName) {
+        return p.getProperty(propertyName);
+    }
+}
